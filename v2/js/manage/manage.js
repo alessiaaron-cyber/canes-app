dd1da99182d21b7724c5cd8fab01879587201bb7
alessiaaron-cyber <alessi.aaron@icloud.com>
2026-05-13 16:22:52 -0400

    Handle Manage subpage account visibility and scroll

--- v2/js/manage/manage.js
+++ v2/js/manage/manage.js
@@ -3,11 +3,34 @@ window.CR = window.CR || {};
 (() => {
   const CR = window.CR;
 
-  function renderManage() {
+  function scrollManageToTop() {
+    const manageView = document.querySelector('#manageView');
+    const appShell = document.querySelector('.app-shell');
+
+    manageView?.scrollTo?.({ top: 0, behavior: 'auto' });
+    appShell?.scrollTo?.({ top: 0, behavior: 'auto' });
+    window.scrollTo?.({ top: 0, behavior: 'auto' });
+  }
+
+  function syncManageChrome() {
+    const accountPanel = document.querySelector('#manageView .account-panel');
+    const isSubpage = CR.manageState?.activeManageView && CR.manageState.activeManageView !== 'main';
+
+    if (accountPanel) {
+      accountPanel.classList.toggle('is-hidden', Boolean(isSubpage));
+    }
+  }
+
+  function renderManage(options = {}) {
     const root = document.querySelector('#manageContent');
     if (!root || !CR.manageRender) return;
 
     root.innerHTML = CR.manageRender.renderRoot(CR.manageState);
+    syncManageChrome();
+
+    if (options.scrollTop) {
+      requestAnimationFrame(scrollManageToTop);
+    }
   }
 
   function initManage() {
@@ -17,5 +40,6 @@ window.CR = window.CR || {};
   }
 
   CR.renderManage = renderManage;
+  CR.scrollManageToTop = scrollManageToTop;
   CR.initManage = initManage;
 })();
